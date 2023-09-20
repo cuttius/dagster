eb6df474ad2e3d3864a1dde8382f8c18de0e2929
Nicholas Schrock <schrockn@dagsterlabs.com>
2023-09-20 12:31:27 -0400

    feedback

--- python_modules/dagster/dagster/_daemon/sensor.py
+++ python_modules/dagster/dagster/_daemon/sensor.py
@@ -679,7 +679,7 @@ def _evaluate_sensor(
 
     assert isinstance(sensor_runtime_data, SensorExecutionData)
 
-    for asset_event in sensor_runtime_data.asset_events or []:
+    for asset_event in sensor_runtime_data.asset_events:
         instance.report_runless_asset_event(asset_event)
 
     if sensor_runtime_data.dynamic_partitions_requests:
